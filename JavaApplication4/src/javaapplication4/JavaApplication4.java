78c87fefba5e93216c6f435dc54a3589ab38edfc
Ntlantla123 <106088782+Ntlantla123@users.noreply.github.com>
2024-05-06 14:16:35 +0200

    Add files via upload

---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author Student
+ */
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+        String[] names = {"Sbusisio", "Nombongo", "Luyolo", "Summer", "Thando"};
+        System.out.println("My name is" + names[3]);
+        System.out.println(names.length);
+        
+        for(int i = 0; i< names.length; i++) {
+            System.out.println();
+            
+            
+            int[][] myNumbers = {{1,2,3,4},{5,6,7,8},{9,10,}};
+            System.out.println(myNumbers[2][1]);
+            
+            
+            
+            
+            
+        }
+    }
+
+}
